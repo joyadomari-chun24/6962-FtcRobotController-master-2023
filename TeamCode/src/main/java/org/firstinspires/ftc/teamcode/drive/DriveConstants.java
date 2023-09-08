6e984939d1b09dacef575e8d11adb4d2916906f6
joyadomari-chun24 <74032103+joyadomari-chun24@users.noreply.github.com>
2023-09-07 19:03:59 -1000

    Adjusted MAX_VEL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,7 +34,7 @@ public class DriveConstants {
      */
     public static final boolean RUN_USING_ENCODER = false;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+            getMotorVelocityF(3.27)); //MAX_RPM / 60 * TICKS_PER_REV
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -65,7 +65,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 61;
+    public static double MAX_VEL = 50.5;
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
