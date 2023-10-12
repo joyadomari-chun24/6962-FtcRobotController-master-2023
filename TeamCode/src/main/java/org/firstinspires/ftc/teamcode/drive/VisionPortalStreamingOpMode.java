675b15b82914bde69fa8c2b909b6f02e4760b34f
joyadomari-chun24 <74032103+joyadomari-chun24@users.noreply.github.com>
2023-10-11 18:50:35 -1000

    OpenCV work 2: electric boogaloo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/VisionPortalStreamingOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/VisionPortalStreamingOpMode.java
@@ -19,6 +19,7 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import org.opencv.core.Core;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -30,12 +31,23 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Autonomous
 public class VisionPortalStreamingOpMode extends LinearOpMode {
     public static class CameraStreamProcessor implements VisionProcessor, CameraStreamSource {
         private final AtomicReference<Bitmap> lastFrame = new AtomicReference<>(Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565));
+
+        static final Rect LEFT_TESTING_RECTANGLE = new Rect(
+                new Point(0, 0),
+                new Point(0, 0));
+
+        static final Rect RIGHT_TESTING_RECTANGLE = new Rect(
+                new Point(0, 0),
+                new Point(0, 0));
+
 
         @Override
         public void init(int width, int height, CameraCalibration calibration) {
@@ -73,8 +85,37 @@ public class VisionPortalStreamingOpMode extends LinearOpMode {
             Mat scaledMask = new Mat();
             imgMask.convertTo(scaledMask, -1, 150/average.val[1], 0);
 
-            imgThresh.copyTo(frame);
-            Imgproc.cvtColor(imgMask, frame, Imgproc.COLOR_HSV2RGB);
+            Scalar strictLowRedHSV = new Scalar(0, 150, 50);
+            Scalar strictHighRedHSV = new Scalar(10, 255, 255);
+
+            Mat scaledThresh = new Mat();
+
+            Core.inRange(scaledMask, strictLowRedHSV, strictHighRedHSV, scaledThresh);
+
+            //Colors in the scaled objects
+            Mat finalMask = new Mat();
+            Core.bitwise_and(imgMat, imgMat, finalMask, scaledThresh);
+
+            //Displays edges
+            Mat imgEdges = new Mat();
+            Imgproc.Canny(scaledThresh, imgEdges, 100, 200);
+
+            //Processes edges somehow (this part is a little mysterious)?
+            List<MatOfPoint> imgContours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(scaledThresh, imgContours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            //Release and return data
+            frame.release();
+            scaledThresh.copyTo(frame);
+            Imgproc.cvtColor(finalMask, frame, Imgproc.COLOR_HSV2RGB);
+            scaledThresh.release();
+            scaledMask.release();
+            imgMat.release();
+            imgMask.release();
+            imgEdges.release();
+            imgThresh.release();
+            finalMask.release();
 
             //Not sure what this does exactly. Removing it makes the camera a black screen on dashboard
             Bitmap b = Bitmap.createBitmap(frame.width(), frame.height(), Bitmap.Config.RGB_565);
