0a0efdf538de0b2a9dd192de9d5162f218ead229
jleung26 <jleung26@punahou.edu>
2023-10-23 16:12:18 -1000

    reverted back to so it works with four bar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Transfer.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.AnalogSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;// switch to iterative later
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Disabled
 public class Transfer extends LinearOpMode { // switch to iterative later
 
     @Override
@@ -14,7 +12,10 @@ public class Transfer extends LinearOpMode { // switch to iterative later
 
         waitForStart();
 
-        // finding motors, servos, and sensors in the hardware map
+        // fraction of 280° that the regular servos need to turn in order to flip
+        double distanceToFlip = 0.6; // tune based on testing
+        // also tune whether it's + or - distanceToFlip
+        // also tune whether it's + or - setPower
         Servo leftFlipServo = hardwareMap.servo.get("leftFlipServo");
         Servo rightFlipServo = hardwareMap.servo.get("rightFlipServo");
         CRServo leftIntakeServo = hardwareMap.crservo.get("leftIntakeServo");
@@ -23,23 +24,17 @@ public class Transfer extends LinearOpMode { // switch to iterative later
         AnalogSensor intakeBreakBeam = hardwareMap.get(AnalogSensor.class, "intakeBreakBeam");
         AnalogSensor scoringBreakBeam = hardwareMap.get(AnalogSensor.class, "scoringBreakBeam");
 
-        // fraction of 280° that the regular servos need to turn in order to flip
-        double distanceToFlip = 0.6; // tune based on testing
-        // also tune whether it's + or - distanceToFlip
-        // also tune whether it's + or - setPower
-
         while (opModeIsActive()) {
             // check if button pressed && other requirements true
             if(gamepad1.y && // y button pressed
-              (intakeBreakBeam.readRawVoltage() < 0.1) && // intake break beam blocked
-              (scoringBreakBeam.readRawVoltage() < 0.1) && // scoring break beam blocked
-              (platformServo.getPosition() < 1 && platformServo.getPosition() > 0.8)) // platform servo in right position
+                    (intakeBreakBeam.readRawVoltage() < 0.1) && // intake break beam blocked
+                    (scoringBreakBeam.readRawVoltage() < 0.1) && // scoring break beam blocked
+                    (platformServo.getPosition() < 1 && platformServo.getPosition() > 0.8)) // platform servo in right position
             {
                 // flips intake upside down
                 leftFlipServo.setPosition(leftFlipServo.getPosition() - distanceToFlip);
                 rightFlipServo.setPosition(rightFlipServo.getPosition() + distanceToFlip);
 
-                // spins until button not pressed (when driver has seen the pixel go into the depositor)
                 while (gamepad1.y) {
                     // spins to eject pixels
                     leftIntakeServo.setPower(1);
