0693fa268bf05499f9057a872a007fdad80fd296
MisterKermit <97013614+MisterKermit@users.noreply.github.com>
2023-11-02 16:55:55 -1000

    changed roadruner consts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -55,7 +55,7 @@ public class DriveConstants {
      * empirically tuned.
      */
     public static double kV = 0.0123; //1.0 / rpmToVelocity(MAX_RPM)
-    public static double kA = 0.0018;
+    public static double kA = 0.002;
     public static double kStatic = 0.10212;
     //to-change, new chasse is constructed
     /*
